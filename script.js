bec1b62f095d7aa8c3c33360d47f409573b2ec7e
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-09-17 23:32:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	
+  if(!str || size<=0)
+  return [];
+  
+  for(let i =0; i<str.length; i+=size) {
+    chunks.push(str.slice(i, i+size));
+  }
+  return chunks;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
