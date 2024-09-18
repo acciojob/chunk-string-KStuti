9d2f22902677e479ae7b881c44e0a57945177bce
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-09-18 12:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function stringChop(str, size) {
 	
   if(!str || size<=0)
   return [];
-  
+
+	const chunks =[];
   for(let i =0; i<str.length; i+=size) {
     chunks.push(str.slice(i, i+size));
   }
